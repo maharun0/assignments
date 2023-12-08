7f8e171942eee8c2bcd5a345836331f8af2780f2
maharun0 <maharun.afroz@northsouth.edu>
2023-12-08 14:32:42 +0600

    solved assignment

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -1,20 +1,43 @@
 /*
   Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
-  transactions is an array where each
-  Transaction - an object like 
-        {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
-  Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
+  Transaction - an object like { itemName, category, price, timestamp }.
+  Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryMap = new Map();
+
+  for (let i = 0; i < transactions.length; i++){
+
+    let tempCat = transactions[i]['category'];
+    let tempPrice = transactions[i]['price'];
+
+    if (categoryMap.has(tempCat)){
+      let prevPrice = categoryMap.get(tempCat);
+      categoryMap.set(tempCat, prevPrice + tempPrice);
+    } else {
+      categoryMap.set(tempCat, tempPrice);
+    }
+  }
+
+  class Category{
+    constructor (category, totalSpent){
+      this.category = category;
+      this.totalSpent = totalSpent ;
+    }
+  }
+  let catArray = [];
+
+  // console.log(categoryMap.length);
+  for (const [key, value] of categoryMap.entries()){
+    catArray.push(new Category(key, value));
+    // console.log(`${key} ${value}`);
+  }
+
+  // console.log(catArray);
+
+  return catArray;
 }
 
 module.exports = calculateTotalSpentByCategory;
